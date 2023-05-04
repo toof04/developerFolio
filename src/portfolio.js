1d9f84aaeb0d69e3366fbf767a57fe5a512ad2dc
Kartik Singh <96460163+toof04@users.noreply.github.com>
2023-05-05 00:49:12 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -32,293 +32,293 @@ const greeting = {
 
 // Social Media Links
 
-const socialMediaLinks = {
-  github: "https://github.com/toof04",
-  linkedin: "linkedin.com/in/kartik-singh-766279246/",
-  gmail: "kartiksingh0412@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
+// const socialMediaLinks = {
+//   github: "https://github.com/toof04",
+//   linkedin: "linkedin.com/in/kartik-singh-766279246/",
+//   gmail: "kartiksingh0412@gmail.com",
+//   gitlab: "https://gitlab.com/saadpasta",
 
-  // Instagram, Twitter and Kaggle are also supported in the links!
-  // To customize icons and social links, tweak src/components/SocialMedia
-  display: true // Set true to display this section, defaults to false
-};
+//   // Instagram, Twitter and Kaggle are also supported in the links!
+//   // To customize icons and social links, tweak src/components/SocialMedia
+//   display: true // Set true to display this section, defaults to false
+// };
 
-// Skills Section
+// // Skills Section
 
-const skillsSection = {
-  title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
-  skills: [
-    emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
-    ),
-    emoji("⚡ Fun and story based solo indie games on playstore and also inwork! "),
-    emoji(
-      "⚡ Integration of third party services such as Firebase and AWS "
-    )
-  ],
+// const skillsSection = {
+//   title: "What I do",
+//   subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+//   skills: [
+//     emoji(
+//       "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+//     ),
+//     emoji("⚡ Fun and story based solo indie games on playstore and also inwork! "),
+//     emoji(
+//       "⚡ Integration of third party services such as Firebase and AWS "
+//     )
+//   ],
 
-  /* Make Sure to include correct Font Awesome Classname to view your icon
-https://fontawesome.com/icons?d=gallery */
+//   /* Make Sure to include correct Font Awesome Classname to view your icon
+// https://fontawesome.com/icons?d=gallery */
 
-  softwareSkills: [
-    {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
-    },
-    {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "JavaScript",
-      fontAwesomeClassname: "fab fa-js"
-    },
-    {
-      skillName: "reactjs",
-      fontAwesomeClassname: "fab fa-react"
-    },
-    {
-      skillName: "nodejs",
-      fontAwesomeClassname: "fab fa-node"
-    },
-    {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
-    },
-    {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
-    },
-    {
-      skillName: "sql-database",
-      fontAwesomeClassname: "fas fa-database"
-    },
-    {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
-      skillName: "Unreal Engine 5",
-      fontAwesomeClassname: "fab fa-Unreal engine 5"
-    },
-    {
-      skillName: "Unity",
-      fontAwesomeClassname: "fab fa-unity"
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
-};
+//   softwareSkills: [
+//     {
+//       skillName: "html-5",
+//       fontAwesomeClassname: "fab fa-html5"
+//     },
+//     {
+//       skillName: "css3",
+//       fontAwesomeClassname: "fab fa-css3-alt"
+//     },
+//     {
+//       skillName: "JavaScript",
+//       fontAwesomeClassname: "fab fa-js"
+//     },
+//     {
+//       skillName: "reactjs",
+//       fontAwesomeClassname: "fab fa-react"
+//     },
+//     {
+//       skillName: "nodejs",
+//       fontAwesomeClassname: "fab fa-node"
+//     },
+//     {
+//       skillName: "swift",
+//       fontAwesomeClassname: "fab fa-swift"
+//     },
+//     {
+//       skillName: "npm",
+//       fontAwesomeClassname: "fab fa-npm"
+//     },
+//     {
+//       skillName: "sql-database",
+//       fontAwesomeClassname: "fas fa-database"
+//     },
+//     {
+//       skillName: "aws",
+//       fontAwesomeClassname: "fab fa-aws"
+//     },
+//     {
+//       skillName: "firebase",
+//       fontAwesomeClassname: "fas fa-fire"
+//     },
+//     {
+//       skillName: "Unreal Engine 5",
+//       fontAwesomeClassname: "fab fa-Unreal engine 5"
+//     },
+//     {
+//       skillName: "Unity",
+//       fontAwesomeClassname: "fab fa-unity"
+//     }
+//   ],
+//   display: true // Set false to hide this section, defaults to true
+// };
 
-// Education Section
+// // Education Section
 
-const educationInfo = {
-  display: true, // Set false to hide this section, defaults to true
-  schools: [
-    {
-      schoolName: "Vellore Institute of Technology",
-     // logo: require("./assets/images/VITLogo.png"),
-      subHeader: "Bachelor of Technology in Computer Science and Engineering",
-      duration: "2020-2024",
-      Grade: "CGPA : 8.17 (Ongoing)",
-    }
-  ]
-};
+// const educationInfo = {
+//   display: true, // Set false to hide this section, defaults to true
+//   schools: [
+//     {
+//       schoolName: "Vellore Institute of Technology",
+//      // logo: require("./assets/images/VITLogo.png"),
+//       subHeader: "Bachelor of Technology in Computer Science and Engineering",
+//       duration: "2020-2024",
+//       Grade: "CGPA : 8.17 (Ongoing)",
+//     }
+//   ]
+// };
 
-// Your top 3 proficient stacks/tech experience
+// // Your top 3 proficient stacks/tech experience
 
-const techStack = {
-  viewSkillBars: true, //Set it to true to show Proficiency Section
-  experience: [
-    {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
-    },
-    {
-      Stack: "Backend",
-      progressPercentage: "70%"
-    },
-    {
-      Stack: "Unity",
-      progressPercentage: "80%"
-    },
-     {
-      Stack: "Unreal Engine 5",
-      progressPercentage: "60%"
-    }
-  ],
-  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
-};
+// const techStack = {
+//   viewSkillBars: true, //Set it to true to show Proficiency Section
+//   experience: [
+//     {
+//       Stack: "Frontend/Design", //Insert stack or technology you have experience in
+//       progressPercentage: "90%" //Insert relative proficiency in percentage
+//     },
+//     {
+//       Stack: "Backend",
+//       progressPercentage: "70%"
+//     },
+//     {
+//       Stack: "Unity",
+//       progressPercentage: "80%"
+//     },
+//      {
+//       Stack: "Unreal Engine 5",
+//       progressPercentage: "60%"
+//     }
+//   ],
+//   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+// };
 
-// Work experience section
+// // Work experience section
 
-const workExperiences = {
-  display: true, //Set it to true to show workExperiences Section
-  experience: [
-    {
-      role: "Web Development Intern",
-      company: "TwiLearn Edutech",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "April 2023 – Present",
-      desc: "Working as a fullstack web developer - completing projects and meeting deadlines :)",
+// const workExperiences = {
+//   display: true, //Set it to true to show workExperiences Section
+//   experience: [
+//     {
+//       role: "Web Development Intern",
+//       company: "TwiLearn Edutech",
+//       companylogo: require("./assets/images/facebookLogo.png"),
+//       date: "April 2023 – Present",
+//       desc: "Working as a fullstack web developer - completing projects and meeting deadlines :)",
  
-    },
-    {
-      role: "Front-End Developer",
-      company: "Wonder Infraestates",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "June 2022– July 2022",
-      desc: "-	Task was to considerably improve their existing website. - Suggested UI/UX changes. -  Worked as a team to respect client views while designing a project."
-    }
-  ]
-};
+//     },
+//     {
+//       role: "Front-End Developer",
+//       company: "Wonder Infraestates",
+//       companylogo: require("./assets/images/quoraLogo.png"),
+//       date: "June 2022– July 2022",
+//       desc: "-	Task was to considerably improve their existing website. - Suggested UI/UX changes. -  Worked as a team to respect client views while designing a project."
+//     }
+//   ]
+// };
 
-/* Your Open Source Section to View Your Github Pinned Projects
-To know how to get github key look at readme.md */
+// /* Your Open Source Section to View Your Github Pinned Projects
+// To know how to get github key look at readme.md */
 
-const openSource = {
-  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
-};
+// const openSource = {
+//   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+//   display: true // Set false to hide this section, defaults to true
+// };
 
-// Some big projects you have worked on
+// // Some big projects you have worked on
 
-const bigProjects = {
-  title: "Big Projects",
-  subtitle: "SOME WORK I AM PROUD OF : ",
-  projects: [
-    {
-      image: require("./assets/images/psy.png"),
-      projectName: "PsyBox - Line Platformer",
-      projectDesc: " Player has to avoid hurdles and  use special abilties inorder to complete the level. ",
-      footerLink: [
-        {
-          name: "Visit Website",
-          url: "https://play.google.com/store/apps/details?id=com.AghoriApps.PsyBox&hl=en_IN&gl=US"
-        }
-        //  you can add extra buttons here.
-      ]
-    },
-    {
-      image: require("./assets/images/treely.png"),
-      projectName: "Treely Ecommerce Website",
-      projectDesc: "Plant nursery ecommerce website for ayurvedic plants",
-      footerLink: [
-        {
-          name: "Visit Website",
-          url: "https://github.com/toof04/Treely"
-        }
-      ]
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
-};
+// const bigProjects = {
+//   title: "Big Projects",
+//   subtitle: "SOME WORK I AM PROUD OF : ",
+//   projects: [
+//     {
+//       image: require("./assets/images/psy.png"),
+//       projectName: "PsyBox - Line Platformer",
+//       projectDesc: " Player has to avoid hurdles and  use special abilties inorder to complete the level. ",
+//       footerLink: [
+//         {
+//           name: "Visit Website",
+//           url: "https://play.google.com/store/apps/details?id=com.AghoriApps.PsyBox&hl=en_IN&gl=US"
+//         }
+//         //  you can add extra buttons here.
+//       ]
+//     },
+//     {
+//       image: require("./assets/images/treely.png"),
+//       projectName: "Treely Ecommerce Website",
+//       projectDesc: "Plant nursery ecommerce website for ayurvedic plants",
+//       footerLink: [
+//         {
+//           name: "Visit Website",
+//           url: "https://github.com/toof04/Treely"
+//         }
+//       ]
+//     }
+//   ],
+//   display: true // Set false to hide this section, defaults to true
+// };
 
-// Achievement Section
-// Include certificates, talks etc
+// // Achievement Section
+// // Include certificates, talks etc
 
-const achievementSection = {
-  title: emoji("Achievements And Certifications 🏆 "),
-  subtitle:
-    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+// const achievementSection = {
+//   title: emoji("Achievements And Certifications 🏆 "),
+//   subtitle:
+//     "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
 
-  achievementsCards: [
-    {
-      title: "Coding Blocks Rank",
-      subtitle:
-        "Global Coding Blocks Rank : 5590",
-      image: require("./assets/images/codingblock.png"),
-      imageAlt: "Coding Block logo",
-    },
-    {
-      title: "Interviewbit Rank",
-      subtitle:
-        "	Interviewbit – Rank 2 on all VIT campuses",
-      image: require("./assets/images/bit.png"),
-      imageAlt: "Interviewbit",
+//   achievementsCards: [
+//     {
+//       title: "Coding Blocks Rank",
+//       subtitle:
+//         "Global Coding Blocks Rank : 5590",
+//       image: require("./assets/images/codingblock.png"),
+//       imageAlt: "Coding Block logo",
+//     },
+//     {
+//       title: "Interviewbit Rank",
+//       subtitle:
+//         "	Interviewbit – Rank 2 on all VIT campuses",
+//       image: require("./assets/images/bit.png"),
+//       imageAlt: "Interviewbit",
 
     
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
-};
+//     }
+//   ],
+//   display: true // Set false to hide this section, defaults to true
+// };
 
-// Blogs Section
+// // Blogs Section
 
-const blogSection = {
-  title: "Blogs",
-  subtitle:
-    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
-  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
-  blogs: [
-    {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-      description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    },
-    {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
-      description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
-    }
-  ],
-  display: false // Set false to hide this section, defaults to true
-};
+// const blogSection = {
+//   title: "Blogs",
+//   subtitle:
+//     "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+//   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+//   blogs: [
+//     {
+//       url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+//       title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+//       description:
+//         "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+//     },
+//     {
+//       url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+//       title: "Why REACT is The Best?",
+//       description:
+//         "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+//     }
+//   ],
+//   display: false // Set false to hide this section, defaults to true
+// };
 
-// Talks Sections
+// // Talks Sections
 
-const talkSection = {
-  title: "TALKS",
-  subtitle: emoji(
-    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
-  ),
+// const talkSection = {
+//   title: "TALKS",
+//   subtitle: emoji(
+//     "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+//   ),
 
-  talks: [
-    {
-      title: "Build Actions For Google Assistant",
-      subtitle: "Codelab at GDG DevFest Karachi 2019",
-      slides_url: "https://bit.ly/saadpasta-slides",
-      event_url: "https://www.facebook.com/events/2339906106275053/"
-    }
-  ],
-  display: false // Set false to hide this section, defaults to true
-};
+//   talks: [
+//     {
+//       title: "Build Actions For Google Assistant",
+//       subtitle: "Codelab at GDG DevFest Karachi 2019",
+//       slides_url: "https://bit.ly/saadpasta-slides",
+//       event_url: "https://www.facebook.com/events/2339906106275053/"
+//     }
+//   ],
+//   display: false // Set false to hide this section, defaults to true
+// };
 
-// Podcast Section
+// // Podcast Section
 
-const podcastSection = {
-  title: emoji("Podcast 🎙️"),
-  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+// const podcastSection = {
+//   title: emoji("Podcast 🎙️"),
+//   subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
 
-  // Please Provide with Your Podcast embeded Link
-  podcast: [
-    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
-  ],
-  display: false // Set false to hide this section, defaults to true
-};
+//   // Please Provide with Your Podcast embeded Link
+//   podcast: [
+//     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+//   ],
+//   display: false // Set false to hide this section, defaults to true
+// };
 
-const contactInfo = {
-  title: emoji("Contact Me ☎️"),
-  subtitle:
-    "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+91 - 9650550279",
-  email_address: "kartiksingh0412@gmail.com"
-};
+// const contactInfo = {
+//   title: emoji("Contact Me ☎️"),
+//   subtitle:
+//     "Discuss a project or just want to say hi? My Inbox is open for all.",
+//   number: "+91 - 9650550279",
+//   email_address: "kartiksingh0412@gmail.com"
+// };
 
-// Twitter Section
+// // Twitter Section
 
-const twitterDetails = {
-  userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: false // Set true to display this section, defaults to false
-};
+// const twitterDetails = {
+//   userName: "twitter", //Replace "twitter" with your twitter username without @
+//   display: false // Set true to display this section, defaults to false
+// };
 
-const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+// const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
 export {
   illustration,
